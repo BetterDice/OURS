4d338fe29a53ede3a0619a7e1a475fbe1c928f8b
Ivan Mokshin <imokshin2003@gmail.com>
2024-06-01 13:16:51 +0300

    Deploy website - based on 025c3a38139a9ff8b23fd39e5846c0e07d5471e2

---
+++ assets/js/4873cc00.ab33c192.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkours=self.webpackChunkours||[]).push([[3827],{9425:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/OURS/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.ae13aa7c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkours=self.webpackChunkours||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
